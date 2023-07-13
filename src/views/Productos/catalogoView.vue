dd74a4c08b91031a0d55fe346f88932517aa67b5
Sara Marquez <ruelasolivia58@gmail.com>
2023-07-12 22:17:35 -0500

    ediciones en la vista de productos

--- src/views/Productos/catalogoView.vue
+++ src/views/Productos/catalogoView.vue
@@ -5,8 +5,8 @@
       <br>
       <div class="row">
         <div class="col-lg-4" v-for="producto in productos" :key="producto.id">
-          <div class="card">
-            <img :src="producto.imagen" class="card-img-top" :alt="producto.nombre">
+          <div class="card card-small">
+            <img :src="producto.imagen"  class="card-img-top custom-image" :alt="producto.nombre">
             <div class="card-body">
               <h5 class="card-title">{{ producto.nombre }}</h5>
               <p class="card-text">{{ producto.descripcion }}</p>
@@ -22,17 +22,17 @@
   const productos = [
     {
       id: 1,
-      nombre: 'Producto 1',
-      descripcion: 'Descripción del producto 1',
-      precio: 10,
-      imagen: 'https://via.placeholder.com/300x200'
+      nombre: 'Cama para Perro',
+      descripcion: 'Resistente, Confortable, con Relleno Suave, Medidas y Color a Elegir (Small, Marino/Gris)',
+      precio: 299,
+      imagen: 'https://m.media-amazon.com/images/I/61rl+xIe2HL._AC_SX679_.jpg'
     },
     {
       id: 2,
-      nombre: 'Producto 2',
-      descripcion: 'Descripción del producto 2',
-      precio: 20,
-      imagen: 'https://via.placeholder.com/300x200'
+      nombre: 'Jaula para Mascota',
+      descripcion: ' hasta 5kg, IATA, Resistente, de fácil transportación',
+      precio: 399.50,
+      imagen: 'https://m.media-amazon.com/images/I/41LbL2i91kL._AC_.jpg'
     },
     {
       id: 3,
@@ -48,5 +48,13 @@
  h1{
     margin: 20px;
  }
+ .card.card-small {
+  max-width: 300px; 
+  max-height: 500px;
+}
+.custom-image {
+  width: 200px; 
+  height: 150px; 
+}
   </style>
   
